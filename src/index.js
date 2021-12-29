87b3aeff2a2b4756ece810f022f98a75c543cd60
RAMAKANT SINGH YADAV <64480297+RSY6520@users.noreply.github.com>
2021-12-30 03:46:26 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Calculator from './App.js';
-import './App.css';
-// import Flames from './code/flames.js';
-// import MyForm from './code/forms';
-
-
-ReactDOM.render(<Calculator />, document.getElementById('root'))
